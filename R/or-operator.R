d4e4a86ad463832fbbc0434a3125e66e45495f8e
David Holstius <david.holstius@gmail.com>
2020-05-13 20:52:59 -0700

    remove flogger dependency

--- R/or-operator.R
+++ R/or-operator.R
@@ -12,22 +12,25 @@
 
 #' @export
 `%or%.cached` <- function (x, y) {
-  root <- attr(x, "root")
-  ext = attr(x, "ext")
-  key <- x
-  verbose <- isTRUE(getOption("cacher.verbose")) || isTRUE(options("verbose"))
+
   msg <- function (...) if(isTRUE(verbose)) message("[%or%.cached] ", ...)
+
+  verbose <- isTRUE(getOption("cacher.verbose")) || isTRUE(options("verbose"))
+
+  root <- attr(x, "root")
+  ext <- attr(x, "ext")
+  key <- x
+
   obj <- NULL; status <- "FAIL"
   if (cache_contains(key, root = root, ext = ext)) {
     status <- "HIT"
     tryCatch(
       obj <- cache_get(key, root = root, ext = ext),
-      error = function (e) flog("error reading from ", key, level = "warn"))
+      error = function (e) warning("error reading from ", key))
   } else {
     status <- "MISS"
     obj <- cache_put(y, key, root = root, ext = ext)
   }
   msg(key, " was a ", status)
-  flog(msg, status, level = "debug")
   return(obj)
 }
